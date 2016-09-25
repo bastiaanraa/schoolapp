9319422f449cc504f3e040dc3381e900abc292c1
baraa <bastiaan.raa@ugent.be>
2016-09-25 15:33:14 +0200

    cleanup

--- content/admin.py
+++ content/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.contrib.flatpages.admin import FlatpageForm, FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
-## OOPS this is a custom widget that works for initializing
-## tinymce instances on stacked and tabular inlines
-## for flatpages, just use the tinymce packaged one.
-#from content.widgets import TinyMCE 
+
 from tinymce.widgets import TinyMCE
 
 
