ce4c7a46aa9b98f2001c274116191ef20b4325cd
baraa <bastiaan.raa@ugent.be>
2016-09-26 20:48:20 +0200

    added loginrequired

--- bestuur/views.py
+++ bestuur/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from bestuur.models import Bestuur
 
-class BestuurDetail(DetailView):
+class BestuurDetail(LoginRequiredMixin, DetailView):
 	model = Bestuur
--- werkgroepen/views.py
+++ werkgroepen/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from werkgroepen.models import Werkgroep
 
-class WerkgroepDetail(DetailView):
+class WerkgroepDetail(LoginRequiredMixin, DetailView):
 	model = Werkgroep
 
 # Create your views here.
