48a6fae098d6f708174483c761b57d2c9ad7cfb8
baraa <bastiaan.raa@ugent.be>
2016-10-20 22:08:08 +0200

    list

--- schoolcalendar/admin.py
+++ schoolcalendar/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import SchoolCalendar
 
-admin.site.register(SchoolCalendar)
+class SchoolCalendarAdmin(admin.ModelAdmin):
+	list_display = ['titel', 'startdatum', 'einddatum', 'get_doelgroep_display', 'publiek']
+
+admin.site.register(SchoolCalendar, SchoolCalendarAdmin)
