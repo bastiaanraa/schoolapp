1329e0354328e77abdb992bc5556b61ffcd20af1
baraa <bastiaan.raa@ugent.be>
2017-09-19 20:42:46 +0200

    Added LogEntryAdmin

--- profile/admin.py
+++ profile/admin.py
@@ -672,3 +672,33 @@ class UserAdmin(ImportMixin, BaseUserAdmin):
 # Re-register UserAdmin
 #admin.site.register(Group)
 admin.site.register(Profile, UserAdmin)
+
+
+from django.contrib.admin.models import LogEntry
+
+class LogEntryAdmin(admin.ModelAdmin):
+    search_fields = (
+        'object_id',
+        'object_repr',
+        'action_time',
+        'action_flag',
+        'change_message',
+    )
+    readonly_fields = ('content_type',
+        'user',
+        'action_time',
+        'object_id',
+        'object_repr',
+        'action_flag',
+        'change_message'
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        actions = super(LogEntryAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+admin.site.register(LogEntry, LogEntryAdmin)
